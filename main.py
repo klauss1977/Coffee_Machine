79ebe41551e4ab57406c4e4662f58c3814f69d37
klauss1977 <klauss1977@yahoo.com>
2023-10-27 16:06:08 +0300

    coffee machine created

---
+++ main.py
@@ -0,0 +1,68 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+def make_drink(drink):
+    water = MENU[drink]['ingredients']['water']
+    coffee = MENU[drink]['ingredients']['coffee']
+    if drink != 'espresso':
+        milk = MENU[drink]['ingredients']['milk']
+        if milk > resources['milk']:
+            print("Sorry there is not enough milk.")
+            return
+        resources['milk'] -= milk
+    if water > resources['water']:
+        print("Sorry there is not enough water.")
+        return
+    elif coffee > resources['coffee']:
+        print("Sorry there is not enough coffee.")
+        return
+    resources['water'] -= water
+    resources['coffee'] -= coffee
+    print(f"Here is your {drink} ☕ . Enjoy!")
+    return resources
+
+
+shutdown = False
+while not shutdown:
+    order = input("What would you like? (espresso/latte/cappuccino): ")
+    if order == 'report':
+        print(f"""Water: {resources['water']} ml
+Milk: {resources['milk']} ml
+Coffee: {resources['coffee']} g
+Money: $""")
+    elif order == 'exit':
+        shutdown = True
+    else:
+        make_drink(order)
+
+print(resources)
